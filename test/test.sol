5d9041245143dc78e9aa9fd2aef36cac7b3cfcf6
ZenX404 <stonewall1933@gmail.com>
2025-03-07 12:26:41 +0800

    学习了solidity中的数据类型，并完成了智能合约数据结构改造

--- test/test.sol
+++ test/test.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
+// struct: 结构体
+// array: 数组
+// mapping: 映射
 contract HelloWorld {
     // 只要是在合约中定义的变量，编译器就会默认将其设置为storage永久性存储，所以不用我们显式声明
     string strVar = "Hello World";
+
+    // 定义结构体
+    struct Info {
+        string phrase;
+        uint256 id;
+        address addr;
+    }
+
+    // 默认storage永久性存储
+    Info[] infos;
+
+    // key和value的名字也可以省略不写
+    mapping(uint id => Info info) infoMapping;
 
     /*
         solidity中有六种存储模式，只要是在智能合约中定义的变量都会以其中的某一种存储模式来进行存储
@@ -21,13 +38,32 @@ contract HelloWorld {
     // 1、读取
     // view关键字：如果函数中只会进行读取操作，不进行修改操作，我们可以给函数声明上view关键字。要养成良好的编程习惯
     // public关键字：可以被外界调用，也可以被内部调用，也可以被继承的子合约调用，拥有最宽泛的调用范围
-    function sayHello() public view returns(string memory) {
-        return addinfo(strVar);
+    function sayHello(uint256 _id) public view returns(string memory) {
+        if (infoMapping[_id].addr == address(0x0)) {
+            return addinfo(strVar); 
+        } else {
+            return addinfo(infoMapping[_id].phrase);
+        }
+
+
+        // for (uint256 i = 0; i < infos.length; i++) {
+        //     if (infos[i].id == _id) {
+        //         return addinfo(infos[i].phrase);
+        //     }
+        // }
+        
     }
 
     // 2、修改
-    function setHelloWorld(string memory newString) public {
-        strVar = newString;
+    // string不是基础数据类型，编译器不会自动帮他加上memory关键字，但是uint256是基础数据类型，所以不需要手动加memory关键字，编译器会自动给它加上
+    // 为了避免入参和其他变量名有冲突，所以把入参的id前面加了一个下划线_
+    function setHelloWorld(string memory newString, uint256 _id) public {
+        // msg是一个环境变量，可以通过msg.send获取这一次调用合约的交易是谁发起的，获取他的地址
+        Info memory info = Info(newString, _id, msg.sender);
+        // 将info写入到mapping中
+        infoMapping[_id] = info;
+        // 把一个结构体赋值给一个数组，这样数据就可以实现永久存储了，可以随时取到相关的info数据
+        // infos.push(info);
     }
 
     // 3、计算
